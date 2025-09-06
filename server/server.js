36390fba7aa145fb8e2cdb6a41062ddbcd561fd3
Burhan Khan <burhankhan@Burhans-Air.attlocal.net>
2025-09-06 09:16:38 -0500

    attempting to redeploy #5

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://your-client-url.netlify.app'],
+    origin: true,  // This allows all origins temporarily
     credentials: true
 }));
 
